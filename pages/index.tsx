12b3224bfae69034e98717fb4b92f84949586b67
kowalsking <kovvalsking@gmail.com>
2022-07-11 23:03:56 +0300

    Classnames

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
-import { Htag } from "../components";
+import { Htag, Button } from "../components";
 
 export default function Home(): JSX.Element {
   return (
     <>
       <Htag tag="h1">Text</Htag>
+      <Button appearance="primary">Button!</Button>
+      <Button appearance="ghost">Button!</Button>
     </>
   );
 }
