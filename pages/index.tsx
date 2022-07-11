9441280e85f762b2c31485e5812c5b41a71187bf
kowalsking <kovvalsking@gmail.com>
2022-07-12 00:13:01 +0300

    Paragpaph component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Htag, Button } from "../components";
+import { Htag, Button, P } from "../components";
 
 export default function Home(): JSX.Element {
   return (
@@ -6,6 +6,7 @@ export default function Home(): JSX.Element {
       <Htag tag="h1">Text</Htag>
       <Button appearance="primary" arrow="right">Button!</Button>
       <Button appearance="ghost">Button!</Button>
+      <P size="l">Oi blyad ebanui React pizdec. Ebanui Frontend yak je ya tebe lyublyu 3</P>
     </>
   );
 }
