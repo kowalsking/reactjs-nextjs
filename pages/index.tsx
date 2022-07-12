c2eb2e7b93b4ffcc7dc199eaede02253ec034bc5
kowalsking <kovvalsking@gmail.com>
2022-07-12 10:21:21 +0300

    useState

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
+import { useState } from 'react';
 import { Htag, Button, P, Tag } from "../components";
 
 export default function Home(): JSX.Element {
+  const [counter, setCounter] = useState<number>(0)
+
   return (
     <>
-      <Htag tag="h1">Text</Htag>
-      <Button appearance="primary" arrow="right">Button!</Button>
+      <Htag tag="h1">{counter}</Htag>
+      <Button appearance="primary" arrow="right" onClick={() => setCounter(x => x+1)}>Button!</Button>
       <Button appearance="ghost">Button!</Button>
       <P size="l">Oi blyad ebanui React pizdec. Ebanui Frontend yak je ya tebe lyublyu 3</P>
       <Tag size="s">small</Tag>
