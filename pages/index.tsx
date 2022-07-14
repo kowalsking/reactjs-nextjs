ed586e5b52dc864e47a3efb3f16358448edd882f
kowalsking <kovvalsking@gmail.com>
2022-07-14 20:54:12 +0300

    High order component Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import { Htag, Button, P, Tag, Rating } from "../components";
-import { Layout } from "../layout/Layout";
+import { withLayout } from "../layout/Layout";
 
-export default function Home(): JSX.Element {
+function Home(): JSX.Element {
   const [rating, setRating] = useState<number>(4);
 
   return (
@@ -26,3 +26,5 @@ export default function Home(): JSX.Element {
     </>
   );
 }
+
+export default withLayout(Home);
