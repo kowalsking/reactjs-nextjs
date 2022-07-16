100c39596390bbf1e2de3d6f8c19f15f8fca4f56
kowalsking <kovvalsking@gmail.com>
2022-07-17 00:53:43 +0300

    Custom context

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,11 +26,6 @@ function Home({ menu }: HomeProps): JSX.Element {
         small
       </Tag>
       <Rating rating={rating} isEditable setRating={setRating} />
-      <ul>
-        {menu.map((m) => (
-          <li key={m._id.secondCategory}>{m._id.secondCategory}</li>
-        ))}
-      </ul>
     </>
   );
 }
