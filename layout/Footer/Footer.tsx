66aba95ef4221e91078ac3fc74b7b2b3ee13e98c
kowalsking <kovvalsking@gmail.com>
2022-07-16 12:57:12 +0300

    Markup footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,7 +1,20 @@
 import { FooterProps } from "./Footer.props";
 import styles from "./Footer.module.css";
 import cn from "classnames";
+import { format } from "date-fns";
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
-  return <div {...props}>Footer</div>;
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+  return (
+    <footer className={cn(className, styles.footer)} {...props}>
+      <div className={styles.copyright}>
+        Copyright всі права захищені (с) 1996-{format(new Date(), "yyyy")}
+      </div>
+      <a href="#" target="_blank" className={styles.userAgreement}>
+        Згода користувача
+      </a>
+      <a href="#" target="_blank" className={styles.confidentialPolitics}>
+        Політика конфіденційності
+      </a>
+    </footer>
+  );
 };
