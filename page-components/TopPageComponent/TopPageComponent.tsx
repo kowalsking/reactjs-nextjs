bc6cfa2977ddd089ad8b5510f578899f7e07dde1
kowalsking <kovvalsking@gmail.com>
2022-08-02 21:25:55 +0300

    Card created

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,9 +1,37 @@
+import { Htag, Tag } from "../../components";
 import { TopPageComponentProps } from "./TopPageComponent.props";
+import styles from "./TopPageComponent.module.css";
 
 export const TopPageComponent = ({
   page,
   products,
   firstCategory,
 }: TopPageComponentProps): JSX.Element => {
-  return <>{products && products.length}</>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.title}>
+        <Htag tag="h1">{page.title}</Htag>
+        {products && (
+          <Tag color="grey" size="m">
+            {products.length}
+          </Tag>
+        )}
+        <span>Сортування</span>
+      </div>
+      {products && products.length}
+      <div>
+        {products && products.map((p) => <div key={p._id}>{p.title}</div>)}
+      </div>
+      <div className={styles.hhTitle}>
+        <Htag tag="h2">Вакансії - {page.category}</Htag>
+        {products && (
+          <Tag color="red" size="m">
+            dou.ua
+          </Tag>
+        )}
+        <span>Сортування</span>
+      </div>
+      <div className={styles.hh}></div>
+    </div>
+  );
 };
