87e6c8d6336346c989420775ff9709fb347e11a3
kowalsking <kovvalsking@gmail.com>
2022-08-04 21:42:06 +0300

    Sort created

---
+++ components/Sort/Sort.tsx
@@ -0,0 +1,9 @@
+import { SortProps } from "./Sort.props";
+import styles from "./Sort.module.css";
+import cn from "classnames";
+
+export const Sort = ({ sort, setSort }: SortProps): JSX.Element => {
+  return (
+    
+  );
+};
