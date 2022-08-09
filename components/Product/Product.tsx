eb9495fa685da4c7d7f1094013c3b0827a4901df
kowalsking <kovvalsking@gmail.com>
2022-08-09 22:13:57 +0300

    Price month rating styles

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -8,10 +8,15 @@ export const Product = ({ product, className, ...props }: ProductProps): JSX.Ele
     <Card className={styles.product}>
       <div className={styles.logo}><img src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title} /></div>
       <div className={styles.title}>{product.title}</div>
-      <div className={styles.price}>{priceUah(product.price)}</div>
-      <div className={styles.credit}>{priceUah(product.credit)}</div>
+      <div className={styles.price}>
+        {priceUah(product.price)}
+        {product.oldPrice && <Tag color="green">{priceUah(product.price - product.oldPrice)}</Tag>}
+      </div>
+      <div className={styles.credit}>
+        {priceUah(product.credit)}/<span className={styles.month}>міс</span>
+      </div>
       <div className={styles.rating}><Rating rating={product.reviewAvg ?? product.initialRating} /></div>
-      <div className={styles.tags}>{product.categories.map(c => <Tag key={c} color='ghost'>{c}</Tag>)}</div>
+      <div className={styles.tags}>{product.categories.map(c => <Tag className={styles.oldPrice} key={c} color='ghost'>{c}</Tag>)}</div>
       <div className={styles.priceTitle}>ціна</div>
       <div className={styles.creditTitle}>кредит</div>
       <div className={styles.rateTitle}>{product.reviewCount} відгуків</div>
