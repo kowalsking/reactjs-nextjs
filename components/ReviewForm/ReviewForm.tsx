c5fb4d84d23331ecf9764892aa849291968d38b1
kowalsking <kovvalsking@gmail.com>
2022-08-23 00:25:43 +0300

    Work with form

--- components/ReviewForm/ReviewForm.tsx
+++ components/ReviewForm/ReviewForm.tsx
@@ -3,6 +3,8 @@ import styles from "./ReviewForm.module.css";
 import cn from "classnames";
 import { Button, Textarea, Rating, Input } from '..';
 import CloseIcon from './close.svg';
+import { useForm, Controller } from 'react-hook-form';
+import { IReviewForm } from './ReviewForm.interface';
 
 export const ReviewForm = ({
   productId,
@@ -10,16 +12,28 @@ export const ReviewForm = ({
   className,
   ...props
 }: ReviewFormProps): JSX.Element => {
+  const { register, control, handleSubmit } = useForm<IReviewForm>()
+
+  const onSubmit = (data: IReviewForm) => {
+    console.log(data)
+  }
+
   return (
-    <>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className={cn(styles.reviewForm, className)} {...props}>
-        <Input placeholder="Ім'я" />
-        <Input placeholder="Заголовок відгуку" className={styles.title} />
+        <Input {...register('name')} placeholder="Ім'я" />
+        <Input {...register('title')} placeholder="Заголовок відгуку" className={styles.title} />
         <div className={styles.rating}>
           <span>Оцінка: </span>
-          <Rating rating={0} />
+          <Controller 
+            control={control}
+            name='rating'
+            render={({ field }) => (
+              <Rating isEditable rating={field.value} setRating={field.onChange} />
+            )}
+          />
         </div>
-        <Textarea placeholder="Текст відгуку" className={styles.description} />
+        <Textarea {...register('description')} placeholder="Текст відгуку" className={styles.description} />
         <div className={styles.submit}>
           <Button appearance="primary">
             Відправити
@@ -38,6 +52,6 @@ export const ReviewForm = ({
         </div>
         <CloseIcon className={styles.close}/>
       </div>
-    </>
+    </form>
   );
 };
