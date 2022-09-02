694ad9346b172b6e9adc5b4a9c94939888a46249
kowalsking <kovvalsking@gmail.com>
2022-09-02 23:51:01 +0300

    Error for textarea

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -5,8 +5,8 @@ import { ForwardedRef, forwardRef } from 'react';
 
 export const Input = forwardRef(({ className, error, ...props }: InputProps, ref: ForwardedRef<HTMLInputElement>): JSX.Element => {
   return (
-    <div className={styles.inputWrapper}>
-      <input className={cn(className, styles.input, {
+    <div className={cn(styles.inputWrapper, className)}>
+      <input className={cn(styles.input, {
         [styles.error]: error
       })} ref={ref} {...props} />
       {error && <span className={styles.errorMessage}>{error.message}</span>}
--- components/ReviewForm/ReviewForm.tsx
+++ components/ReviewForm/ReviewForm.tsx
@@ -1,10 +1,10 @@
 import { ReviewFormProps } from "./ReviewForm.props";
 import styles from "./ReviewForm.module.css";
 import cn from "classnames";
-import { Button, Textarea, Rating, Input } from '..';
-import CloseIcon from './close.svg';
-import { useForm, Controller } from 'react-hook-form';
-import { IReviewForm } from './ReviewForm.interface';
+import { Button, Textarea, Rating, Input } from "..";
+import CloseIcon from "./close.svg";
+import { useForm, Controller } from "react-hook-form";
+import { IReviewForm } from "./ReviewForm.interface";
 
 export const ReviewForm = ({
   productId,
@@ -12,54 +12,70 @@ export const ReviewForm = ({
   className,
   ...props
 }: ReviewFormProps): JSX.Element => {
-  const { register, control, handleSubmit, formState: { errors } } = useForm<IReviewForm>()
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IReviewForm>();
 
   const onSubmit = (data: IReviewForm) => {
-    console.log(data)
-  }
+    console.log(data);
+  };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className={cn(styles.reviewForm, className)} {...props}>
         <Input
-          {...register('name', { required: { value: true, message: "Вкажіть своє ім'я"}})}
+          {...register("name", {
+            required: { value: true, message: "Вкажіть своє ім'я" },
+          })}
           placeholder="Ім'я"
           error={errors.name}
         />
         <Input
-          {...register('title', { required: { value: true, message: "Вкажіть заголвок"}})}
+          {...register("title", {
+            required: { value: true, message: "Вкажіть заголвок" },
+          })}
           placeholder="Заголовок відгуку"
           className={styles.title}
           error={errors.title}
         />
         <div className={styles.rating}>
           <span>Оцінка: </span>
-          <Controller 
+          <Controller
             control={control}
-            name='rating'
+            name="rating"
             render={({ field }) => (
-              <Rating isEditable rating={field.value} ref={field.ref} setRating={field.onChange} />
+              <Rating
+                isEditable
+                rating={field.value}
+                ref={field.ref}
+                setRating={field.onChange}
+              />
             )}
           />
         </div>
-        <Textarea {...register('description')} placeholder="Текст відгуку" className={styles.description} />
+        <Textarea
+          {...register("description", {
+            required: { value: true, message: "Вкажіть свою думку" },
+          })}
+          placeholder="Текст відгуку"
+          className={styles.description}
+          error={errors.description}
+        />
         <div className={styles.submit}>
-          <Button appearance="primary">
-            Відправити
-          </Button>
+          <Button appearance="primary">Відправити</Button>
           <span className={styles.info}>
-            * Перед публікацією переконайтесь що ви ненавидите русню і донатите на ЗСУ
+            * Перед публікацією переконайтесь що ви ненавидите русню і донатите
+            на ЗСУ
           </span>
         </div>
       </div>
       <div className={styles.success}>
-        <div className={styles.successTitle}>
-          Ваш відгук надіслано
-        </div>
-        <div>
-          Дякуємо, руский корабель іде нахуй!
-        </div>
-        <CloseIcon className={styles.close}/>
+        <div className={styles.successTitle}>Ваш відгук надіслано</div>
+        <div>Дякуємо, руский корабель іде нахуй!</div>
+        <CloseIcon className={styles.close} />
       </div>
     </form>
   );
