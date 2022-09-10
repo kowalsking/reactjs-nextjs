6179d1e887b9797cc827b06576f894461bfe7ab9
kowalsking <kovvalsking@gmail.com>
2022-09-10 20:37:14 +0300

    Gestures and motionValues

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,23 +1,43 @@
 import { ButtonProps } from "./Button.props";
 import styles from "./Button.module.css";
 import cn from "classnames";
-import ArrowIcon from './arrow.svg'
+import ArrowIcon from "./arrow.svg";
+import { motion, useMotionValue } from "framer-motion";
+import { useEffect } from "react";
 
-export const Button = ({ appearance, arrow = 'none', children, className, ...props }: ButtonProps): JSX.Element => {
+export const Button = ({
+  appearance,
+  arrow = "none",
+  children,
+  className,
+  ...props
+}: ButtonProps): JSX.Element => {
+  const scale = useMotionValue(1);
+
+  useEffect(() => {
+    scale.onChange((s) => console.log(s));
+  }, []);
+
   return (
-    <button
+    <motion.button
+      whileHover={{ scale: 1.05 }}
       className={cn(styles.button, className, {
         [styles.primary]: appearance === "primary",
         [styles.ghost]: appearance === "ghost",
       })}
+      style={{ scale }}
       {...props}
     >
       {children}
-      {arrow !== 'none' && <span className={cn(styles.arrow, {
-        [styles.down]: arrow === 'down',
-      })}>
-        <ArrowIcon />
-        </span>}
-    </button>
+      {arrow !== "none" && (
+        <span
+          className={cn(styles.arrow, {
+            [styles.down]: arrow === "down",
+          })}
+        >
+          <ArrowIcon />
+        </span>
+      )}
+    </motion.button>
   );
 };
