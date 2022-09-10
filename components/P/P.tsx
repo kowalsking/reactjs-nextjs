75e77db07c10811ad886552cfa08d1200d82131f
kowalsking <kovvalsking@gmail.com>
2022-09-11 00:27:39 +0300

    A11Y COLOR

--- components/P/P.tsx
+++ components/P/P.tsx
@@ -2,13 +2,18 @@ import { PProps } from "./P.props";
 import styles from "./P.module.css";
 import cn from "classnames";
 
-export const P = ({ size = 'm', children, className, ...props }: PProps): JSX.Element => {
+export const P = ({
+  size = "m",
+  children,
+  className,
+  ...props
+}: PProps): JSX.Element => {
   return (
-    <p 
+    <p
       className={cn(styles.paragraph, className, {
-        [styles.s]: size === 's',
-        [styles.m]: size === 'm',
-        [styles.l]: size === 'l',
+        [styles.s]: size === "s",
+        [styles.m]: size === "m",
+        [styles.l]: size === "l",
       })}
       {...props}
     >
