466c989a684973a748018062418c6bc4379365d8
kowalsking <kovvalsking@gmail.com>
2022-09-11 12:31:04 +0300

    a11y sorting

--- components/Sort/Sort.tsx
+++ components/Sort/Sort.tsx
@@ -1,27 +1,32 @@
 import { SortEnum, SortProps } from "./Sort.props";
 import styles from "./Sort.module.css";
-import SortIcon from "./sort.svg"
+import SortIcon from "./sort.svg";
 import cn from "classnames";
 
-export const Sort = ({ sort, setSort, className, ...props }: SortProps): JSX.Element => {
+export const Sort = ({
+  sort,
+  setSort,
+  className,
+  ...props
+}: SortProps): JSX.Element => {
   return (
     <div className={cn(styles.sort, className)} {...props}>
-      <span
+      <button
         onClick={() => setSort(SortEnum.Rating)}
         className={cn({
-          [styles.active]: sort === SortEnum.Rating
+          [styles.active]: sort === SortEnum.Rating,
         })}
       >
         <SortIcon className={styles.sortIcon} /> За рейтингом
-      </span>
-      <span
+      </button>
+      <button
         onClick={() => setSort(SortEnum.Price)}
         className={cn({
-          [styles.active]: sort === SortEnum.Price
+          [styles.active]: sort === SortEnum.Price,
         })}
       >
         <SortIcon className={styles.sortIcon} /> За ціною
-      </span>
+      </button>
     </div>
   );
 };
