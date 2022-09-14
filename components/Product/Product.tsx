1c1bbabcae4759ebd779faef3fab6a9b1022e5aa
kowalsking <kovvalsking@gmail.com>
2022-09-14 21:37:29 +0300

    Aria-hidden

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -43,18 +43,24 @@ export const Product = motion(
             </div>
             <div className={styles.title}>{product.title}</div>
             <div className={styles.price}>
+              <span className="visuallyHidden">Ціна</span>
               {priceUah(product.price)}
               {product.oldPrice && (
                 <Tag className={styles.oldPrice} color="green">
+                  <span className="visuallyHidden">знижка</span>
                   {priceUah(product.price - product.oldPrice)}
                 </Tag>
               )}
             </div>
             <div className={styles.credit}>
+              <span className="visuallyHidden">кредит</span>
               {priceUah(product.credit)}/
               <span className={styles.month}>міс</span>
             </div>
             <div className={styles.rating}>
+              <span className="visuallyHidden">
+                {"рейтинг" + (product.reviewAvg ?? product.initialRating)}
+              </span>
               <Rating rating={product.reviewAvg ?? product.initialRating} />
             </div>
             <div className={styles.tags}>
@@ -64,8 +70,12 @@ export const Product = motion(
                 </Tag>
               ))}
             </div>
-            <div className={styles.priceTitle}>ціна</div>
-            <div className={styles.creditTitle}>кредит</div>
+            <div className={styles.priceTitle} aria-hidden={true}>
+              ціна
+            </div>
+            <div className={styles.creditTitle} aria-hidden={true}>
+              кредит
+            </div>
             <div className={styles.rateTitle}>
               <a href="#ref" onClick={scrollToReview}>
                 {product.reviewCount}{" "}
