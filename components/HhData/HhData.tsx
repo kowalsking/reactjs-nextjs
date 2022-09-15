ad0805d35aeade35ee500434da2c15452435ed71
kowalsking <kovvalsking@gmail.com>
2022-09-16 00:41:57 +0300

    HhData rm classname import

--- components/HhData/HhData.tsx
+++ components/HhData/HhData.tsx
@@ -1,11 +1,15 @@
 import styles from "./HhData.module.css";
-import cn from "classnames";
 import { HhDataProps } from "./HhData.props";
 import { Card } from "..";
-import RateIcon from './rate.svg';
-import { priceUah } from '../../helpers/helpers';
+import RateIcon from "./rate.svg";
+import { priceUah } from "../../helpers/helpers";
 
-export const HhData = ({ count, juniorSalary, middleSalary, seniorSalary }: HhDataProps): JSX.Element => {
+export const HhData = ({
+  count,
+  juniorSalary,
+  middleSalary,
+  seniorSalary,
+}: HhDataProps): JSX.Element => {
   return (
     <div className={styles.hh}>
       <Card className={styles.count}>
@@ -17,27 +21,27 @@ export const HhData = ({ count, juniorSalary, middleSalary, seniorSalary }: HhDa
           <div className={styles.title}>Початковий</div>
           <div className={styles.salaryValue}>{priceUah(juniorSalary)}</div>
           <div className={styles.rate}>
-            <RateIcon className={styles.filled} />  
-            <RateIcon />  
-            <RateIcon />  
+            <RateIcon className={styles.filled} />
+            <RateIcon />
+            <RateIcon />
           </div>
         </div>
         <div>
           <div className={styles.title}>Середній</div>
           <div className={styles.salaryValue}>{priceUah(middleSalary)}</div>
           <div className={styles.rate}>
-            <RateIcon className={styles.filled} />  
-            <RateIcon className={styles.filled} />  
-            <RateIcon />  
+            <RateIcon className={styles.filled} />
+            <RateIcon className={styles.filled} />
+            <RateIcon />
           </div>
         </div>
         <div>
           <div className={styles.title}>Професіонал</div>
           <div className={styles.salaryValue}>{priceUah(seniorSalary)}</div>
           <div className={styles.rate}>
-            <RateIcon className={styles.filled} />  
-            <RateIcon className={styles.filled} />  
-            <RateIcon className={styles.filled} />  
+            <RateIcon className={styles.filled} />
+            <RateIcon className={styles.filled} />
+            <RateIcon className={styles.filled} />
           </div>
         </div>
       </Card>
