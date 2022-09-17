47b5a01e32b647f811db14f44a02141962f70892
kowalsking <kovvalsking@gmail.com>
2022-09-17 23:18:49 +0300

    SUUUIIII

--- helpers/helpers.tsx
+++ helpers/helpers.tsx
@@ -1,10 +1,9 @@
-
 import CoursesIcon from "./icons/courses.svg";
 import ServicesIcon from "./icons/services.svg";
 import BooksIcon from "./icons/books.svg";
 import ProductsIcon from "./icons/products.svg";
-import { FirstLevelMenuItem } from '../interfaces/menu.interface';
-import { TopLevelCategory } from '../interfaces/page.interface';
+import { FirstLevelMenuItem } from "../interfaces/menu.interface";
+import { TopLevelCategory } from "../interfaces/page.interface";
 
 export const firstLevelMenu: FirstLevelMenuItem[] = [
   {
@@ -31,11 +30,22 @@ export const firstLevelMenu: FirstLevelMenuItem[] = [
     icon: <ProductsIcon />,
     id: TopLevelCategory.Products,
   },
-]
+];
 
-export const priceUah = (price: number): string => price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ').concat(' ₴')
+export const priceUah = (price: number): string =>
+  price
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, " ")
+    .concat(" ₴");
 
-export const declOfNum = (number: number, titles: [string, string, string]): string => {
-  const cases = [2, 0, 1, 1, 1, 2]
-  return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]]
-} 
+export const declOfNum = (
+  number: number,
+  titles: [string, string, string]
+): string => {
+  const cases = [2, 0, 1, 1, 1, 2];
+  return titles[
+    number % 100 > 4 && number % 100 < 20
+      ? 2
+      : cases[number % 10 < 5 ? number % 10 : 5]
+  ];
+};
